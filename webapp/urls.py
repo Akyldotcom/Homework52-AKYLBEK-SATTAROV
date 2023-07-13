8d7b922611ef4267ea11a9ae4a233cc2702b0aac
Akyl <akyl.ktl@mail.ru>
2023-07-14 01:04:42 +0600

    поменял на generic вюшки

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,13 +3,13 @@ from django.views.generic import RedirectView
 
 
 from webapp.views.views import \
-     ArticleListView, ArticleCreateView, ArticleDView, article_update_view, article_delete_view
+    ArticleListView, ArticleCreateView, ArticleDView,  ArticleDeleteView, ArticleUpdateView
 
 urlpatterns = [
     path('', RedirectView.as_view(pattern_name="index")),
     path('articles/', ArticleListView.as_view(), name="index"),
     path('articles/add/', ArticleCreateView.as_view(), name="article_add"),
     path('article/<int:pk>/', ArticleDView.as_view(), name="article_view"),
-    path('article/<int:pk>/update/', article_update_view, name="article_update_view"),
-    path('article/<int:pk>/delete/', article_delete_view, name="article_delete_view")
+    path('article/<int:pk>/update/', ArticleUpdateView.as_view(), name="article_update_view"),
+    path('article/<int:pk>/delete/', ArticleDeleteView.as_view(), name="article_delete_view")
 ]
--- webapp/views/views.py
+++ webapp/views/views.py
@@ -1,8 +1,8 @@
 from django.db.models import Q
-from django.shortcuts import render, redirect, reverse, get_object_or_404
-from django.http import HttpResponseRedirect, Http404, HttpResponseNotFound
-from django.views import View
-from django.views.generic import TemplateView, FormView, ListView
+from django.urls import reverse_lazy
+from django.utils.html import urlencode
+from django.shortcuts import redirect, get_object_or_404
+from django.views.generic import TemplateView, FormView, ListView, DeleteView, UpdateView
 
 from webapp.forms import ArticleForm, SearchForm
 from webapp.models import Article
@@ -12,7 +12,7 @@ class ArticleListView(ListView):
     model = Article
     template_name = "articles/index.html"
     context_object_name = "articles"
-    ordering = ("updated_at",)
+    ordering = ("-updated_at",)
     paginate_by = 5
 
     def dispatch(self, request, *args, **kwargs):
@@ -24,6 +24,7 @@ class ArticleListView(ListView):
         context = super().get_context_data(object_list=None, **kwargs)
         context["form"] = self.form
         if self.search_value:
+            context["query"] = urlencode({"search": self.search_value})
             context["search_value"] = self.search_value
         return context
 
@@ -32,7 +33,7 @@ class ArticleListView(ListView):
 
     def get_search_value(self):
         if self.form.is_valid():
-            return self.form.cleaned_data['search']
+            return self.form.cleaned_data["search"]
         return None
 
     def get_queryset(self):
@@ -42,10 +43,6 @@ class ArticleListView(ListView):
             queryset = queryset.filter(Q(title__icontains=self.search_value) |
                                        Q(author__icontains=self.search_value))
         return queryset
-
-    # def  get_queryset(self):
-    #     queryset= super().get_queryset()
-    #     return queryset.filter(title__contains="A")
 
 
 class ArticleCreateView(FormView):
@@ -75,71 +72,79 @@ class ArticleCreateView(FormView):
 #         return render(request, "create_article.html", {"form": form})
 
 
-class ArticleUpdateView(FormView):
+class ArticleUpdateView(UpdateView):
+    model = Article
     form_class = ArticleForm
     template_name = "articles/update_article.html"
+    success_url = reverse_lazy("your-success-url")
+    # def __init__(self, **kwargs):
+    #     super().__init__(kwargs)
+    #     self.article = None
+    #
+    # def dispatch(self, request, *args, **kwargs):
+    #     self.article = self.get_object(kwargs.get("pk"))
+    #     return super().dispatch(request, *args, **kwargs)
+    #
+    # def get_object(self, pk):
+    #     return get_object_or_404(Article, id=pk)
+    #
+    # def get_form_kwargs(self):
+    #     kwargs = super().get_form_kwargs()
+    #     kwargs['instance'] = self.article
+    #     return kwargs
+    #
+    # def form_valid(self, form):
+    #     form.save()
+    #     return redirect("article_view", pk=self.article.pk)
+    #
+    #
 
-    def __init__(self, **kwargs):
-        super().__init__(kwargs)
-        self.article = None
-
-    def dispatch(self, request, *args, **kwargs):
-        self.article = self.get_object(kwargs.get("pk"))
-        return super().dispatch(request, *args, **kwargs)
-
-    def get_object(self, pk):
-        return get_object_or_404(Article, id=pk)
-
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        kwargs['instance'] = self.article
-        return kwargs
-
-    def form_valid(self, form):
-        form.save()
-        return redirect("article_view", pk=self.article.pk)
-
-
-def article_update_view(request, pk):
-    article = get_object_or_404(Article, id=pk)
-    if request.method == "GET":
-        form = ArticleForm(initial={
-            "title": article.title,
-            "author": article.author,
-            "content": article.content,
-            "genres": article.genres.all()
-        })
-        return render(request, "articles/update_article.html", {"form": form})
-    else:
-        form = ArticleForm(data=request.POST)
-        if form.is_valid():
-            genres = form.cleaned_data.pop("genres")
-            article.title = form.cleaned_data.get("title")
-            article.content = form.cleaned_data.get("content")
-            article.author = form.cleaned_data.get("author")
-            article.save()
-            article.genres.set(genres)
-            return redirect("article_view", pk=article.pk)
-        else:
-            return render(request, "articles/update_article.html", {"form": form})
-
-
-def article_view(request, *args, pk, **kwrags):
-    # article = get_object_or_404(Article, id=pk)
-    try:
-        article = Article.objects.get(id=pk)
-    except Article.DoesNotExist:
-        return HttpResponseNotFound("Not found ")
-    return render(request, "articles/article.html", {"article": article})
+#
+# def article_update_view(request, pk):
+#     article = get_object_or_404(Article, id=pk)
+#     if request.method == "GET":
+#         form = ArticleForm(initial={
+#             "title": article.title,
+#             "author": article.author,
+#             "content": article.content,
+#             "genres": article.genres.all()
+#         })
+#         return render(request, "articles/update_article.html", {"form": form})
+#     else:
+#         form = ArticleForm(data=request.POST)
+#         if form.is_valid():
+#             genres = form.cleaned_data.pop("genres")
+#             article.title = form.cleaned_data.get("title")
+#             article.content = form.cleaned_data.get("content")
+#             article.author = form.cleaned_data.get("author")
+#             article.save()
+#             article.genres.set(genres)
+#             return redirect("article_view", pk=article.pk)
+#         else:
+#             return render(request, "articles/update_article.html", {"form": form})
 
 
-def article_delete_view(request, pk):
-    article = get_object_or_404(Article, id=pk)
-    if request.method == "GET":
-        return render(request, "articles/delete_article.html", {"article": article})
-    else:
-        article.delete()
-        return redirect("index")
+# def article_view(request, *args, pk, **kwrags):
+#     # article = get_object_or_404(Article, id=pk)
+#     try:
+#         article = Article.objects.get(id=pk)
+#     except Article.DoesNotExist:
+#         return HttpResponseNotFound("Not found ")
+#     return render(request, "articles/article.html", {"article": article})
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = "articles/delete_article.html"
+    success_url = reverse_lazy("index")
+
+
+# def article_delete_view(request, pk):
+#     article = get_object_or_404(Article, id=pk)
+#     if request.method == "GET":
+#         return render(request, "articles/delete_article.html", {"article": article})
+#     else:
+#         article.delete()
+#         return redirect("index")
 
 
 class ArticleDView(TemplateView):
